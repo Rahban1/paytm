d2bdaabf5913fe4f1e48443dd15c8a90d30b1515
rahban <rahban.ghani2001@gmail.com>
2024-05-27 00:58:44 +0530

    fin ch

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,7 @@ app.use(express.json())
 
 app.use("/api/v1",rootRouter);
 
-app.listen(3000);
-
-
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
